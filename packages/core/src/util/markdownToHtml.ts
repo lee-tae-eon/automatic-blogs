f49cb51459e887adb1f85fdc3249059073654c63
Lee Tae Eon <mktears0609@gmail.com>
2026-01-18 10:28:04 +0900

    ERR: 동적 import 수정

---
+++ packages/core/src/util/markdownToHtml.ts
@@ -0,0 +1,12 @@
+export async function markdownToHtml(markdown: string): Promise<string> {
+  // 동적 임포트를 사용하여 ESM 전용 라이브러리를 로드합니다.
+  const { unified } = await import("unified");
+  const { default: remarkParse } = await import("remark-parse");
+  const { default: remarkHtml } = await import("remark-html");
+
+  const result = await unified()
+    .use(remarkParse)
+    .use(remarkHtml)
+    .process(markdown);
+  return result.toString();
+}
