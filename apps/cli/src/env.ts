a34e89e442924480cd5ec60dfec7da9fa5641089
Lee Tae Eon <mktears0609@gmail.com>
2026-01-18 10:30:56 +0900

    ADD: path 수정

--- apps/cli/src/env.ts
+++ apps/cli/src/env.ts
@@ -2,10 +2,7 @@ import dotenv from "dotenv";
 import path from "path";
 
 dotenv.config({
-  path: [
-    path.join(__dirname, "../../../.env.local"),
-    path.join(__dirname, "../../../.env"),
-  ],
+  path: [path.join(__dirname, "../../../.env")],
 });
 
 export const ENV = {
